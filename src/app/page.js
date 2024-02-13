35686955c0c0b0419f0a61a8654b15be8dd4e289
Souvik <souvikmandal210@gmail.com>
2024-02-13 11:50:48 +0530

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,17 @@
+"use client"
 import Image from "next/image";
-import {Hero} from "./../components/hero"
+import { Hero } from "./../components/hero"
+
+import * as React from "react";
+
+// 1. import `NextUIProvider` component
+import { NextUIProvider } from "@nextui-org/react";
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <NextUIProvider>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">  </main>
+    </NextUIProvider>
   );
 }
